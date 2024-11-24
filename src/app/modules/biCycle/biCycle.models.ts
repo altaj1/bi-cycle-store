53732e3f08315682900388413d51afbfcfca2c9d
altaj1 <altaj1019@gmail.com>
2024-11-24 15:37:34 +0600

    add Mongoose schema validation

--- src/app/modules/biCycle/biCycle.models.ts
+++ src/app/modules/biCycle/biCycle.models.ts
@@ -3,17 +3,25 @@ import { ETcycletype, TbiCycle } from './biCycle.interface';
 
 const bicycleSchema = new Schema<TbiCycle>(
   {
-    name: { type: String, required: true },
-    brand: { type: String, required: true },
-    price: { type: Number, required: true },
+    name: { type: String, required: [true, 'Bicycle name is required'] },
+    brand: { type: String, required: [true, 'Brand is required'] },
+    price: {
+      type: Number,
+      required: true,
+      min: [0, 'Quantity cannot be negative'],
+    },
     type: {
       type: String,
       enum: ETcycletype,
       required: true,
     },
-    description: { type: String, required: true },
-    quantity: { type: Number, required: true },
-    inStock: { type: Boolean, required: true },
+    description: { type: String, required: [true, 'Descriotuion is required'] },
+    quantity: {
+      type: Number,
+      required: true,
+      min: [0, 'Quantity cannot be negative'],
+    },
+    inStock: { type: Boolean, required: true, default: true },
   },
   { timestamps: true },
 );
